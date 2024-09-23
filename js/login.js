5270ab642b4c6b5b7a0046682e2dae1721a579e0
Sabbir-663 <ahamedsabbir663@gmail.com>
2024-09-23 06:35:48 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,19 @@
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber=document.getElementById('phone-number').value ;
+    const pinNumber=document.getElementById('pin-number').value ;
+    //console.log(phoneNumber,pinNumber);
+
+    if (phoneNumber==="0123456789" && pinNumber==="2024"){
+        console.log('You are Welcome !!!! ^_^');
+        window.location.href ='/home.html'
+        
+    }
+    else{
+        alert('Ke Tumi? ');
+        
+    }
+
+
+})
